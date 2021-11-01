e4bf2743e2b174e0c5a65438b0988a0a95ae25b6
Chris Wheeler <grintor@gmail.com>
2021-11-01 19:03:46 -0400

    .

---
+++ complex.py
@@ -0,0 +1,24 @@
+def f10():
+    """McCabe rating: 11"""
+    myint = 2
+    if myint == 5:
+        return myint
+    elif myint == 6:
+        return myint
+    elif myint == 7:
+        return myint
+    elif myint == 8:
+        return myint
+    elif myint == 9:
+        return myint
+    elif myint == 10:
+        if myint == 8:
+            while True:
+                return True
+        elif myint == 8:
+                return 8
+    else:
+        if myint == 2:
+            return myint
+        return myint
+    return myint
