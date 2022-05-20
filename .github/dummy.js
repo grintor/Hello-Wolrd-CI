e08546645a16b10a846646f40472d971a8485013
Chris Wheeler <grintor@gmail.com>
2022-05-20 11:01:32 -0400

    . (#79)

--- .github/dummy.js
+++ .github/dummy.js
@@ -1,2 +1,2 @@
 "use strict";
-console.log("This is a dummy file to make the CodeQL job not crash. Do not delete");
+console.log("This is a dummy file to make the CodeQL job not crash. Do not delete.");
