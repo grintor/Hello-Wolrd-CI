486908243be4c5bd1c504aaca0d78ae4285660ec
Chris Wheeler <grintor@gmail.com>
2022-05-24 14:22:25 -0400

    tmp

--- tests/hello_world_test.py
+++ tests/hello_world_test.py
@@ -4,4 +4,5 @@ import src.hello_world
 def test_hello_world(capsys):
     src.hello_world.main()
     captured = capsys.readouterr()
+    src.hello_world.foo(1.5)
     assert captured.out == "Hello world!\n"
